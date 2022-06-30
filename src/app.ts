a789c8db214ff673e533488249b021f42f9faeb6
yuval <nhajeho1@gmail.com>
2022-06-30 23:15:56 +0300

    added search functionallity

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
 import { Component } from "./components/component";
-import { getPokemonList } from "./pokemonApi";
+import { getPokemonComponents, getPokemonNames } from "./pokemonApi";
 
 function renderComponentList(components: Component[], listParent: HTMLElement) {
+	listParent.innerHTML = "";
 	for (const component of components) {
 		listParent.appendChild(component.createHtml());
 	}
 }
 
-getPokemonList(0, 9).then((pokemonList) =>
+getPokemonComponents(0, 15).then((pokemonList) =>
 	renderComponentList(
 		pokemonList,
 		document.getElementsByClassName("pokemon-list")[0] as HTMLElement
 	)
+);
+
+(document.querySelector(".search-button") as HTMLElement).addEventListener(
+	"click",
+	(e) => {
+		const searchQuery = (
+			(e.target as HTMLElement).previousSibling as HTMLInputElement
+		).value;
+		getPokemonNames((pokemons) => {
+			getPokemonComponents(
+				0,
+				0,
+				pokemons.filter((item: any) => item.name.includes(searchQuery))
+			).then((pokemonList) =>
+				renderComponentList(
+					pokemonList,
+					document.getElementsByClassName("pokemon-list")[0] as HTMLElement
+				)
+			);
+		});
+	}
 );
