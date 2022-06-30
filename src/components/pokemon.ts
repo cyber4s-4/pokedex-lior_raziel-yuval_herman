784542813bed7d940c08d3d63206b359ecc18f71
yuval <nhajeho1@gmail.com>
2022-06-30 12:46:13 +0300

    made scss for pokemon component

--- src/components/pokemon.ts
+++ src/components/pokemon.ts
@@ -1,27 +1,26 @@
 import { Component } from "./component";
 
 export class Pokemon implements Component {
-  name: string;
-  img: string;
-  description: string;
-  constructor(name: string, img: string, description: string) {
-    this.name = name;
-    this.img = img;
-    this.description = description;
-  }
+	name: string;
+	img: string;
+	description: string;
+	constructor(name: string, img: string, description: string) {
+		this.name = name;
+		this.img = img;
+		this.description = description;
+	}
 
-  createHtml(): HTMLElement {
-    const containerDiv = document.createElement("div");
-    containerDiv.classList.add("pokemon-component");
-    containerDiv.innerHTML = 
-	` <div class="flex-right">
+	createHtml(): HTMLElement {
+		const containerDiv = document.createElement("div");
+		containerDiv.classList.add("pokemon-component");
+		containerDiv.innerHTML = ` <div class="flex-right">
 	<img src="${this.img}" alt="A ${this.name} image">
 	<div class="flex-down">
-	<h4> ${this.name}</h4>
-	<div class="flex-down"></div>
+	<h1 class="pokemon-name"> ${this.name}</h1>
+	<div class="flex-down">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates perspiciatis corrupti neque sed in possimus eum voluptatum quae, quam, deleniti quisquam atque exercitationem adipisci magnam omnis perferendis suscipit aspernatur labore?</div>
 	</div>
 	</div>
 	<p class="pokemon-description">${this.description}</p>`;
-    return containerDiv;
-  }
+		return containerDiv;
+	}
 }
