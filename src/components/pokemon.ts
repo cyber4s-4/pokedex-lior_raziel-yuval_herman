3a956a333ab4db528a5030c782f9ee5f267ad35d
razijun <liorazi5@gmail.com>
2022-06-30 12:08:22 +0300

    implement component for pokemon and creating html func

--- src/components/pokemon.ts
+++ src/components/pokemon.ts
@@ -1,10 +1,27 @@
-export class Pokemon {
-	name: string;
-	img: string;
-	description: string;
-	constructor(name: string, img: string, description: string) {
-		 this.name = name;
-		 this.img = img;
-		 this.description = description;
-	}
+import { Component } from "./component";
+
+export class Pokemon implements Component {
+  name: string;
+  img: string;
+  description: string;
+  constructor(name: string, img: string, description: string) {
+    this.name = name;
+    this.img = img;
+    this.description = description;
+  }
+
+  createHtml(): HTMLElement {
+    const containerDiv = document.createElement("div");
+    containerDiv.classList.add("pokemon-component");
+    containerDiv.innerHTML = 
+	` <div class="flex-right">
+	<img src="${this.img}" alt="A ${this.name} image">
+	<div class="flex-down">
+	<h4> ${this.name}</h4>
+	<div class="flex-down"></div>
+	</div>
+	</div>
+	<p class="pokemon-description">${this.description}</p>`;
+    return containerDiv;
+  }
 }
