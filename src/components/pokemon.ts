2c73ae942a07787a0e27f405e5b8267574bc329d
yuval <nhajeho1@gmail.com>
2022-06-30 22:09:43 +0300

    changed id="statsDiv" to  class="stats-div" in pokemon component

--- src/components/pokemon.ts
+++ src/components/pokemon.ts
@@ -1,42 +1,46 @@
 import { Component } from "./component";
 
 export class Pokemon implements Component {
-  name: string;
-  img: string;
-  description: string;
-  stats: object[];
-  constructor(name: string, img: string, description: string, stats: Object[]) {
-    this.name = name;
-    this.img = img;
-    this.description = description;
-    this.stats = stats;
-    // console.log(this.stats);
-  }
+	name: string;
+	img: string;
+	description: string;
+	stats: object[];
+	constructor(
+		name: string,
+		img: string,
+		description: string,
+		stats: Object[]
+	) {
+		this.name = name;
+		this.img = img;
+		this.description = description;
+		this.stats = stats;
+		// console.log(this.stats);
+	}
 
-  createHtml(): HTMLElement {
-    const stats = this.stats.map((element: any) => {
-      return `${element.stat.name} is ${element.base_stat} at ${element.effort} effort`;
-    });
-	
-	
-    const containerDiv = document.createElement("div");
-    containerDiv.classList.add("pokemon-component");
-    containerDiv.innerHTML = ` <div class="flex-right">
+	createHtml(): HTMLElement {
+		const stats = this.stats.map((element: any) => {
+			return `${element.stat.name} is ${element.base_stat} at ${element.effort} effort`;
+		});
+
+		const containerDiv = document.createElement("div");
+		containerDiv.classList.add("pokemon-component");
+		containerDiv.innerHTML = ` <div class="flex-right">
 	<img class="pokemon-image" src="${this.img}" alt="A ${this.name} image">
 	<div class="flex-down">
 	<h1 class="pokemon-name"> ${this.name}</h1>
-	<div id="statsDiv" class="flex-down"></div>
+	<div class="stats-div" class="flex-down"></div>
 	</div>
 	</div>
 	<p class="pokemon-description">${this.description}</p>`;
-	
-	for (let i = 0; i < stats.length; i++) {
-		const statsParagraph = document.createElement("p");
-		statsParagraph.innerHTML = stats[i];
-		containerDiv.querySelector("#statsDiv")?.appendChild(statsParagraph);
+
+		for (let i = 0; i < stats.length; i++) {
+			const statsParagraph = document.createElement("p");
+			statsParagraph.innerHTML = stats[i];
+			containerDiv.querySelector(".stats-div")?.appendChild(statsParagraph);
+		}
+		return containerDiv;
 	}
-    return containerDiv;
-  }
 }
 // for (let j = 0; j < stats.length; j++) {
 // 	const baseStat = stats[j].base_stat;
