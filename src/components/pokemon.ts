9d31cc15c33aa51922d7cf5f5ecf510dd7940e97
yuval <nhajeho1@gmail.com>
2022-06-30 14:26:28 +0300

    shorten lorem ipsum filler

--- src/components/pokemon.ts
+++ src/components/pokemon.ts
@@ -14,13 +14,13 @@ export class Pokemon implements Component {
 		const containerDiv = document.createElement("div");
 		containerDiv.classList.add("pokemon-component");
 		containerDiv.innerHTML = ` <div class="flex-right">
-	<img src="${this.img}" alt="A ${this.name} image">
-	<div class="flex-down">
-	<h1 class="pokemon-name"> ${this.name}</h1>
-	<div class="flex-down">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates perspiciatis corrupti neque sed in possimus eum voluptatum quae, quam, deleniti quisquam atque exercitationem adipisci magnam omnis perferendis suscipit aspernatur labore?</div>
-	</div>
-	</div>
-	<p class="pokemon-description">${this.description}</p>`;
+        <img src="${this.img}" alt="A ${this.name} image">
+        <div class="flex-down">
+        <h1 class="pokemon-name"> ${this.name}</h1>
+        <div class="flex-down">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates perspiciatis</div>
+        </div>
+        </div>
+        <p class="pokemon-description">${this.description}</p>`;
 		return containerDiv;
 	}
 }
