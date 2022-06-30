d341b7c2f1d373480025fabd087e9a7812479a1f
yuval <nhajeho1@gmail.com>
2022-06-30 12:45:56 +0300

    fomratting pass

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { Pokemon } from "./components/pokemon";
 
-const pokemon = new Pokemon("pikachu",
- "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg","hashmali")
+const pokemon = new Pokemon(
+	"pikachu",
+	"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+	"hashmali"
+);
 const baseUrl = "https://pokeapi.co/api/v2/";
 
 const pokemonList = [];
@@ -9,8 +12,6 @@ const pokemonList = [];
 fetch(baseUrl + "pokemon")
 	.then((response) => response.json())
 	.then((data) => {
-		const body = document.body;
 		pokemonList.push(...data.results);
 	});
 document.body.appendChild(pokemon.createHtml());
-
