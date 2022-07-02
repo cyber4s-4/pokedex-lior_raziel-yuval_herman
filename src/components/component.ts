500511607a10f63480638bf7a23daf5dff0cbeca
yuval <nhajeho1@gmail.com>
2022-07-02 23:36:51 +0300

    removed unused component

--- src/components/component.ts
+++
@@ -1,3 +0,0 @@
-export interface Component {
-	createHtml(): HTMLElement;
-}
--- src/components/pokemon.ts
+++ src/components/pokemon.ts
@@ -1,6 +1,4 @@
-import { Component } from "./component";
-
-export class Pokemon implements Component {
+export class Pokemon {
 	name: string;
 	img: string;
 	description: string;
