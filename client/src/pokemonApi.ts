904733d035450e6310ae917833f501576ab117d6
yuval <nhajeho1@gmail.com>
2022-07-07 20:06:19 +0300

    refactor client api calls to work with new server

--- client/src/pokemonApi.ts
+++ client/src/pokemonApi.ts
@@ -1,102 +1,16 @@
 import { Pokemon } from "./components/pokemon";
 
-const baseUrl = "https://pokeapi.co/api/v2/";
-
-export interface PokemonName {
-	name: string;
-	url: string;
-}
-
-export interface PokemonExtraDetails {
-	color: string;
-	height: string;
-	weight: string;
-	category: string;
-	abilities: string[];
-}
+const baseUrl = "https://localhost:3000/";
 
 export async function fetchJson(url: string) {
 	return await (await fetch(url)).json();
 }
 
-/**
- * Initiates fetches for pokemon names from the api
- * @param offset Offset from names array beginning
- * @param limit Amount of names to load
- * @returns A Promise list with pokemon names
- */
-export async function getPokemonNameList(
-	offset: number = 0,
-	limit: number = 20
-): Promise<PokemonName[]> {
-	return (
-		await fetchJson(`${baseUrl}pokemon/?limit=${limit}&offset=${offset}`)
-	).results;
-}
-
-/**
- * Tries to load images and uses several fallbacks if image is unavailable
- * @param spritesObj A sprite object received from the API
- * @returns String or undefined
- */
-function resolveImage(spritesObj: any): string | undefined {
-	return (
-		spritesObj.other.dream_world.front_default ||
-		spritesObj.other["official-artwork"].front_default ||
-		spritesObj.front_default
-	);
-}
-
-/**
- * Creates a promise that will resolve to a Pokemon Object after API fetches.
- * @param pokemonNameObj A pokemon name object from the API
- * @returns A Pokemon promise
- */
-async function makePokemonPromise(pokemonNameObj: PokemonName) {
-	const name = pokemonNameObj.name;
-	const pokemonData = await fetchJson(pokemonNameObj.url);
-	const species = await fetchJson(pokemonData.species.url);
-	const stats = pokemonData.stats;
-	const extraData: PokemonExtraDetails = {
-		color: species.color.name,
-		height: pokemonData.height,
-		abilities: pokemonData.abilities.map((e: any) => e.ability.name),
-		category: species.genera.find((e: any) => e.language.name === "en").genus,
-		weight: pokemonData.weight,
-	};
-	return new Pokemon(
-		name,
-		resolveImage(pokemonData.sprites),
-		species.flavor_text_entries.find(
-			(element: any) => element.language.name === "en"
-		).flavor_text,
-		stats,
-		extraData
-	);
-}
-
-/**
- * Initiates fetches for pokemon' and returns a promise array
- * @param offset Offset from array beginning
- * @param limit Amount of pokemon's to load
- * @param pokemonNameList A list of pokemon names to use, if not provided, fetch from server using offset and limit
- * @returns Pokemon promise array
- */
-export async function getPokemonComponents(
-	offset: number = 0,
-	limit: number = 20,
-	pokemonNameList: null | PokemonName[] = null
-) {
-	pokemonNameList =
-		pokemonNameList?.slice(offset, limit) ??
-		(await PokemonNamesPromise()).slice(offset, limit);
-	const pokemonList: Promise<Pokemon>[] = [];
-	for (let i = 0; i < pokemonNameList.length; i++) {
-		pokemonList.push(makePokemonPromise(pokemonNameList[i]));
+export async function getPokemonComponents() {
+	const pokemonObjects = await fetchJson(baseUrl);
+	const pokemonComponents: Pokemon[] = [];
+	for (const pokemon of pokemonObjects) {
+		pokemonComponents.push(new Pokemon(pokemon));
 	}
-	return pokemonList;
+	return pokemonComponents;
 }
-
-const promise = getPokemonNameList(0, 99999);
-// A promise that resolves to all pokemon names
-export const PokemonNamesPromise = promise.then.bind(promise);
