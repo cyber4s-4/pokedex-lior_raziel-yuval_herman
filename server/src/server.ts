b4379ed514077c9fa1bdd52ce9bc4fe9c47f13c3
yuval <nhajeho1@gmail.com>
2022-07-13 20:29:24 +0300

    made client static files be served with the api server

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 interface User {
 	name: string;
