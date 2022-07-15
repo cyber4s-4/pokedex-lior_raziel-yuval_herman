ce21cdd9fb55caad01f99e3ad0b620c71ce0180c
razijun <liorazi5@gmail.com>
2022-07-15 09:28:39 +0300

    offset and limit client side

--- client/src/pokemonApi.ts
+++ client/src/pokemonApi.ts
@@ -1,25 +1,23 @@
 import { Pokemon } from "./components/pokemon";
 
 const baseUrl =
-	(window.location.port === "4000" ? "http://localhost:3000/" : "/") +
-	"pokemons";
-const pokemonComponents: Pokemon[] = [];
+  (window.location.port === "4000" ? "http://localhost:3000/" : "/") +
+  "pokemons";
 
 export async function fetchJson(url: string) {
-	return await (await fetch(url)).json();
+  return await (await fetch(url)).json();
 }
 
 export async function getPokemonComponents(
-	offset: number = 0,
-	limit: number = Infinity
+  offset: number = 0,
+  limit: number = Infinity
 ) {
-	if (pokemonComponents.length)
-		return pokemonComponents.slice(offset, limit + offset);
-
-	const pokemonObjects = (await fetchJson(baseUrl)) as object[];
-
-	for (const pokemon of pokemonObjects) {
-		pokemonComponents.push(new Pokemon(pokemon));
-	}
-	return pokemonComponents.slice(offset, limit + offset);
+  const pokemonObjects = (await fetchJson(
+    baseUrl + `?limit=${limit}&offset=${offset}`
+  )) as object[];
+  const pokemonComponents = [];
+  for (const pokemon of pokemonObjects) {
+    pokemonComponents.push(new Pokemon(pokemon));
+  }
+  return pokemonComponents.slice(offset, limit + offset);
 }
