0b76271311c6766025ccbbb1e5e58cbf357e8ef1
yuval <nhajeho1@gmail.com>
2022-07-15 10:59:05 +0300

    added getPokemonNames function

--- client/src/pokemonApi.ts
+++ client/src/pokemonApi.ts
@@ -1,23 +1,34 @@
 import { Pokemon } from "./components/pokemon";
 
 const baseUrl =
-  (window.location.port === "4000" ? "http://localhost:3000/" : "/") +
-  "pokemons";
+	(window.location.port === "4000" ? "http://localhost:3000/" : "/") +
+	"pokemons";
+
+let pokemonsNames: string[] = [];
 
 export async function fetchJson(url: string) {
-  return await (await fetch(url)).json();
+	return await (await fetch(url)).json();
 }
 
 export async function getPokemonComponents(
-  offset: number = 0,
-  limit: number = Infinity
+	offset: number = 0,
+	limit: number = Infinity
 ) {
-  const pokemonObjects = (await fetchJson(
-    baseUrl + `?limit=${limit}&offset=${offset}`
-  )) as object[];
-  const pokemonComponents = [];
-  for (const pokemon of pokemonObjects) {
-    pokemonComponents.push(new Pokemon(pokemon));
-  }
-  return pokemonComponents.slice(offset, limit + offset);
+	const pokemonObjects = (await fetchJson(
+		baseUrl + `?limit=${limit}&offset=${offset}`
+	)) as object[];
+	const pokemonComponents = [];
+	for (const pokemon of pokemonObjects) {
+		pokemonComponents.push(new Pokemon(pokemon));
+	}
+	return pokemonComponents.slice(offset, limit + offset);
+}
+
+export async function getPokemonNames() {
+	if (!pokemonsNames.length) {
+		pokemonsNames = (await fetchJson(baseUrl)).map(
+			(doc: { _id: string; name: string }) => doc.name
+		);
+	}
+	return pokemonsNames;
 }
